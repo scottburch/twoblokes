418a2c3565c53a3b392d8a7018db6de2a63e615a
scott <scott@bulldoginfo.com>
2019-07-02 17:58:53 -0700

    added boat icon to where-are-we-now

--- components/map/Map.js
+++ components/map/Map.js
@@ -1,9 +1,13 @@
 import React, {useEffect, createRef} from 'react'
-import {Map, View} from 'ol'
+import {Map, View, Feature} from 'ol'
 import {OSM} from 'ol/source'
-import {Tile as TileLayer} from 'ol/layer'
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer'
 import {fromLonLat} from 'ol/proj'
 import 'ol/ol.css'
+import {Vector as VectorSource} from 'ol/source'
+import {Point} from 'ol/geom'
+import boatIcon from './boat-icon.png'
+import {Icon, Style} from 'ol/style.js';
 
 const lonLat = [-123.5, 49.5];
 
@@ -12,10 +16,26 @@ export default () => {
 
     useEffect(() => {
             const map = new Map({
-                target: 'where-are-we-now-map',
+                target: mapRef.current,
                 layers: [
                     new TileLayer({
                         source: new OSM()
+                    }),
+                    new VectorLayer({
+                        source: new VectorSource({
+                            features: [
+                                new Feature({
+                                    type: 'icon',
+                                    geometry: new Point(fromLonLat(lonLat)),
+                                })
+                            ]
+                        }),
+                        style: new Style({
+                            image: new Icon({
+                                src: boatIcon
+                            })
+                        })
+
                     })
                 ],
                 view: new View({
@@ -27,7 +47,7 @@ export default () => {
 
     return (
         <>
-            <div id="where-are-we-now-map" ref={mapRef} />
+            <div ref={mapRef} />
         </>
     )
 };
