3056844422c2de75b932414ac2b13dc37ca7c770
scott <scott@bulldoginfo.com>
2019-07-03 07:30:53 -0700

    rebuild to fix map

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3124b6e9dd7ef28b09ab";
+/******/ 	var hotCurrentHash = "1ef9a728ee6dfa758db9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
