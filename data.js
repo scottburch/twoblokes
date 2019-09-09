b7a0aff583e3538eba06f95cfd190e23ed339ead
scott <scott@bulldoginfo.com>
2019-09-08 18:42:29 -0700

    Changed location

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const data = {
     currentLocation: {
-        lat: '48:59.640',
-        lon: '-123:-47.521',
-        description: 'In Ladysmith, BC doing some work'
+        lat: '48:41.249',
+        lon: '-123:-28.791',
+        description: 'In Deep Cove on Vancouver Island for the night'
     }
 };
