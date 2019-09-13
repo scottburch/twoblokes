8903e2c0bd0244f33e35f5cb8c891099c2188eac
scott <scott@bulldoginfo.com>
2019-09-12 22:28:59 -0700

    update location

--- data.js
+++ data.js
@@ -2,6 +2,6 @@ export const data = {
     currentLocation: {
         lat: '48:7.188',
         lon: '-123:-25.549',
-        description: 'Quarantine Cove waiting to enter the USA tomorrow!!'
+        description: 'In Port Angeles, WA USA'
     }
 };
