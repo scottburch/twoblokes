ef1ee585560a57bac2e81476447d136c9219568b
scott <scott@bulldoginfo.com>
2019-10-03 20:07:20 -0700

    update

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const data = {
     currentLocation: {
         lat: '39:25.735',
         lon: '-123:-48.709',
-        description: 'Anchored near Fort Brag for the night.  Winds and waves were coming up more than we wanted to deal with at night',
+        description: 'Anchored near Fort Bragg for the night.  Winds and waves were coming up more than we wanted to deal with at night',
         updatedOn: 'October 3rd'
     }
 };
